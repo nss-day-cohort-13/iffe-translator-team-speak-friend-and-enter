62092d2d2390abc3b9cc8b7dba2b09f80172ea7e
Damon Romano <rdromano@mac.com>
2016-04-27 09:49:46 -0500

    On team review, fixed syntax in last key:value pair.

--- latin.js
+++ latin.js
@@ -2,7 +2,7 @@ var translate = function(translate) {
 
 	var lexicon = {
 		Merry Christmas: "felicem natalem Christi",
-		In the morning, look west. An old guy in a white dress will come over the hill or something.  -Joe Shepherd: "Mane , occidentem respicit . Guido vetus vestis candida vel veniet super collem.  -Joe pastor",
+		In the morning, look west. An old guy in a white dress will come over the hill or something.  -Joe Shepherd: "Mane , occidentem respicit . Guido vetus vestis candida vel veniet super collem.  -Joe pastor"
 	}
 
 translate.latin = function(inputWord) {
