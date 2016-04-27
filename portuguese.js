1564e3deaa7ea8fb95bb93f9a94a3d371dd23c6e
pbhowelljr <pbhowelljr@gmail.com>
2016-04-26 22:30:40 -0500

    debug translate/translate augmenter iife's

--- portuguese.js
+++ portuguese.js
@@ -1,12 +1,14 @@
-var translate = (function(translate) {
-  var lexicon = {
-    keyOne: valueOne,
-    keyTwo: valueTwo,
-    keyThree: valueThree,
-  }
+var translate = function(translate) {
+	
+	var lexicon = {
+		keyOne: 'valueOne',
+		keyTwo: 'valueTwo',
+		keyThree: 'valueThree'
+	};
 
 translate.portuguese = function(inputWord) {
-  return lexicon.inputWord
+	return(lexicon[inputWord]);
 }
-  return translate;
-}(translate));
+
+return translate;
+}(translate);
