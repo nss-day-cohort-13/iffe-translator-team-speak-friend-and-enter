d3e879f6cb608b85e2e82ee0ad2cd1096484e382
Damon Romano <rdromano@mac.com>
2016-04-27 09:21:56 -0500

    entered key: value pairs to lati.js

--- latin.js
+++ latin.js
@@ -1,8 +1,7 @@
 var translate = (function(translate) {
 	var lexicon = {
-		keyOne: valueOne,
-		keyTwo: valueTwo,
-		keyThree: valueThree,
+		Merry Christmas: "felicem natalem Christi",
+		In the morning, look west. An old guy in a white dress will come over the hill or something.  -Joe Shepherd: "Mane , occidentem respicit . Guido vetus vestis candida vel veniet super collem.  -Joe pastor",
 	}
 
 translate.latin = function(inputWord) {
