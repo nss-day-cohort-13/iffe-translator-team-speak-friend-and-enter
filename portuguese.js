9f88a07ab98af0af9cc52ba6f1366b447c7bf158
Melanie Baker <melanierbaker@yahoo.com>
2016-04-27 09:57:16 -0500

    added key values

--- portuguese.js
+++ portuguese.js
@@ -1,8 +1,7 @@
 var translate = (function(translate) {
   var lexicon = {
-    keyOne: valueOne,
-    keyTwo: valueTwo,
-    keyThree: valueThree,
+    merry christmas: "feliz natal",
+    Look to my coming, at first light, on the fifth day. At dawn, look to the East: "Espere minha chegada, na primeira luz, do quinto dia. Aõ amanhecer, olhe para o leste",
   }
 
 translate.portuguese = function(inputWord) {
