4c837a47dfa8f9aff070fb0b9bff39a46d0ca815
Melanie Baker <melanierbaker@yahoo.com>
2016-04-27 11:13:43 -0500

    finished portuguese lexicon object

--- portuguese.js
+++ portuguese.js
@@ -1,8 +1,9 @@
-var translate = (function(translate) {
+var translate = function(translate) {
+
   var lexicon = {
-    merry christmas: "feliz natal",
-    Look to my coming, at first light, on the fifth day. At dawn, look to the East: "Espere minha chegada, na primeira luz, do quinto dia. Aõ amanhecer, olhe para o leste"
-  }
+    "merry christmas": "feliz natal",
+    "happy new year": "feliz ano novo"
+  };
 
 translate.portuguese = function(inputWord) {
 	return(lexicon[inputWord]);
