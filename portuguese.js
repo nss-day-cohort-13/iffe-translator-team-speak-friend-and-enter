f3581c85f352f881c0d91963f1d6d5d2aa6e209d
Melanie Baker <melanierbaker@yahoo.com>
2016-04-27 10:04:34 -0500

    fixed syntax on lexicon object

--- portuguese.js
+++ portuguese.js
@@ -1,7 +1,7 @@
 var translate = (function(translate) {
   var lexicon = {
     merry christmas: "feliz natal",
-    Look to my coming, at first light, on the fifth day. At dawn, look to the East: "Espere minha chegada, na primeira luz, do quinto dia. Aõ amanhecer, olhe para o leste",
+    Look to my coming, at first light, on the fifth day. At dawn, look to the East: "Espere minha chegada, na primeira luz, do quinto dia. Aõ amanhecer, olhe para o leste"
   }
 
 translate.portuguese = function(inputWord) {
